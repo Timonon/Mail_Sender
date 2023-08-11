844d29e6e7b737223bb1da6b64b92e2f56debdb6
IhorKrav <102153718+Timonon@users.noreply.github.com>
2023-08-11 10:52:58 +0300

    files for the program

---
+++ Mail_Sender/Login_Window.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.CodeDom;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+
+using Sender_Screen;
+namespace Mail_Sender
+{
+    public partial class Login_Window : Window
+    {
+        
+        string login;
+        string password;
+        public Login_Window()
+        {
+            InitializeComponent();
+        }
+        private void GetVariables()
+        {
+            login = tbLogin.Text;
+            
+            password = tbPassword.Text;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetVariables();
+            if (Validate_Email_Adress(login))
+            {
+                if (Valide_Password(password))
+                {
+                    MailWindow send = new MailWindow(login, password);
+                    send.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid email address");
+            }
+        }
+        private bool Validate_Email_Adress(string mail)
+        {
+            if(string.IsNullOrEmpty(mail))
+                return false;//empty field
+            try
+            {
+                MailAddress address = new MailAddress(mail);
+                return true;//valid
+            }
+            catch (FormatException)
+            {
+                return false;//invalid
+            }
+
+        }
+        private bool Valide_Password(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            if(password.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Mail_Sender/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Win32;
+using Mail_Sender;
+namespace Sender_Screen
+{
+    public partial class MailWindow : Window
+    {
+        MimeMessage message;
+        OpenFileDialog dialog;
+        MessageImportance importance;
+        string user;
+        string password;
+        public MailWindow(string login, string password)
+        {
+            importance = MessageImportance.Normal;
+            message = new MimeMessage();    
+            dialog = new OpenFileDialog();
+            InitializeComponent();
+            this.user = login;
+            this.password = password;   
+        }
+
+        
+        private MimeMessage Make_Message()
+        {
+            message.From.Add(new MailboxAddress(user, user));
+            message.To.Add(new MailboxAddress("User", tbSendTo.Text));
+            message.Subject = tbTheme.Text;
+            message.Importance = importance;
+            message.Body = Make_Message_Body().ToMessageBody();
+            return message;
+        }
+
+        private BodyBuilder Make_Message_Body()
+        {
+                BodyBuilder builder = new BodyBuilder();
+            if (dialog.FileNames == null)
+            {
+                builder.TextBody = $@"{tbMessage.Text}";
+
+                return builder;
+            }
+            else
+            {
+                foreach (var item in dialog.FileNames)
+                {
+                builder.Attachments.Add(item);
+                }
+                builder.TextBody = $@"{tbMessage.Text}";
+
+                return builder;
+            }
+        }
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect);
+                try
+                {
+                client.Authenticate(user, password);
+                    client.Send(Make_Message());
+                    MessageBox.Show("Letter sent succesfully", "Succes");
+
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Failed to send (check your password or resivers email)", "Fail");
+                }
+            }
+        }
+
+
+
+        private void Attach_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+            dialog.Multiselect = true;
+
+            dialog.ShowDialog();
+
+        }
+
+        private void Set_Priority_Button_Click(object sender, RoutedEventArgs e)
+        {
+            importance = MessageImportance.High;
+        }
+    }
+}
